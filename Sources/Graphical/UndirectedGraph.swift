10242df00190d6db0fb3d0f69728267fdc9b8cb9
Robert Bigelow <rfbigelow@mac.com>
2022-04-30 15:28:37 -0700

    Add UndirectedGraph

---
+++ Sources/Graphical/UndirectedGraph.swift
@@ -0,0 +1,39 @@
+//
+//  UndirectedGraph.swift
+//  
+//
+//  Created by Robert Bigelow on 4/30/22.
+//
+
+public struct UndirectedGraph {
+    private var vertices: [[Int]]
+    public private(set) var edgeCount: Int
+    
+    public init(vertexCount: Int) {
+        precondition(vertexCount > 0)
+        vertices = Array(repeating: [], count: vertexCount)
+        edgeCount = 0
+    }
+    
+    public mutating func addEdge(u: Int, v: Int) {
+        precondition(u >= 0 && u < vertices.count)
+        precondition(v >= 0 && v < vertices.count)
+        guard !vertices[u].contains(v) else {
+            return
+        }
+        vertices[u].append(v)
+        vertices[v].append(u)
+        edgeCount += 1
+    }
+}
+
+extension UndirectedGraph: Graph {
+    public var vertexCount: Int {
+        return vertices.count
+    }
+    
+    public func adjacent(v: Int) -> [Int] {
+        precondition(v >= 0 && v < vertexCount)
+        return vertices[v]
+    }
+}
