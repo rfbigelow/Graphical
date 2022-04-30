b190e5de1d9d651caf2015833aad7beb8b3eeadd
Robert Bigelow <rfbigelow@mac.com>
2022-04-30 15:06:00 -0700

    Move protocol conformance to extension

--- Sources/Graphical/Digraph.swift
+++ Sources/Graphical/Digraph.swift
@@ -6,13 +6,8 @@
 //
 
 /// A directed graph.
-public struct Digraph: Graph {
+public struct Digraph {
     private var vertices: [[Int]]
-    
-    /// Gets the number of vertices in the graph.
-    public var vertexCount: Int {
-        return vertices.count
-    }
     
     /// Gets the number of edges in the graph.
     public private(set) var edgeCount: Int = 0
@@ -25,6 +20,28 @@ public struct Digraph: Graph {
         vertices = Array(repeating: [], count: vertexCount)
     }
     
+    /// Adds a directed  edge from `u` to `v` to the graph.
+    ///
+    ///  - Parameters:
+    ///     - u: The source vertex, which must be greater than or equal to `0` and less than `vertexCount`.
+    ///     - v: The target vertex, which must be greater than or equal to `0` and less than `vertexCount`.
+    public mutating func addEdge(u: Int, v: Int) {
+        precondition(u >= 0 && u < vertices.count)
+        precondition(v >= 0 && v < vertices.count)
+        guard !vertices[u].contains(v) else {
+            return
+        }
+        vertices[u].append(v)
+        edgeCount += 1
+    }
+}
+
+extension Digraph: Graph {
+    /// Gets the number of vertices in the graph.
+    public var vertexCount: Int {
+        return vertices.count
+    }
+   
     /// Gets an array containing the vertices that are adjacent to `v`.
     ///
     /// - Parameters:
@@ -45,20 +62,5 @@ public struct Digraph: Graph {
             }
         }
         return g
-    }
-    
-    /// Adds a directed  edge from `u` to `v` to the graph.
-    ///
-    ///  - Parameters:
-    ///     - u: The source vertex, which must be greater than or equal to `0` and less than `vertexCount`.
-    ///     - v: The target vertex, which must be greater than or equal to `0` and less than `vertexCount`.
-    public mutating func addEdge(u: Int, v: Int) {
-        precondition(u >= 0 && u < vertices.count)
-        precondition(v >= 0 && v < vertices.count)
-        guard !vertices[u].contains(v) else {
-            return
-        }
-        vertices[u].append(v)
-        edgeCount += 1
     }
 }
